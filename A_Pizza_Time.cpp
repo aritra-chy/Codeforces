6c3967785610ef985943a1da64c27361d3b63508
aritra-chy <seoaritra2002@gmail.com>
2025-10-25 17:02:23 +0600

    committed

---
+++ A_Pizza_Time.cpp
@@ -0,0 +1,35 @@
+/*------------------------------------------------------
+    author    : Aritra Chowdhury
+    created   : Saturday | 25 October,2025 | 16:56:06 
+------------------------------------------------------*/
+ 
+#include <bits/stdc++.h>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+using namespace std;
+#define read(type) readInt<type>() // Fast read
+#define ll long long
+#define nl "\n"
+#define vi vector<int>
+#define vll vector<long long>
+#define vs vector<string>
+#define MOD 1000000007
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+ 
+int main()
+{
+  fast;
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    cout<<(n-1)/2<<nl;
+  }
+  return 0;
+}
